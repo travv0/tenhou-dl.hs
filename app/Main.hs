b57a3a84d354861191a800fa1b6b33312f19c453
Travis <tssund93@gmail.com>
2021-01-14 21:13:21 -0600

    refactor to use wreq instead of req

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import           Lib
+import Lib
 
-import qualified Data.Text                     as T
-import           System.Environment
+import qualified Data.Text as T
+import System.Environment
 
 version :: String
 version = "1.0.2"
@@ -12,15 +12,16 @@ main :: IO ()
 main = do
   putStrLn $ "tenhou-dl v" ++ version
   args <- getArgs
-  if length args /= 2
-    then
-      putStrLn
-      $  "Usage: tenhou-dl <Tenhou ID> <Log path>\n"
-      ++ "Example: tenhou-dl ID12345678-6fnB8AoP \"C:\\tenhou\\logs\\\""
-    else do
-      let (tenhouId : path : _) = args
-      tags  <- getTags (TenhouID $ T.pack tenhouId)
+  case args of
+    [tenhouId, path] -> do
+      tags <- getTags (TenhouID $ T.pack tenhouId)
       count <- length <$> downloadReplays (parseDownloadUrls tags) path
-      putStrLn $ "\nDownloaded " ++ show count ++ " replay" ++ if count /= 1
-        then "s"
-        else ""
+      putStrLn $
+        "\nDownloaded " ++ show count ++ " replay"
+          ++ if count /= 1
+            then "s"
+            else ""
+    _ ->
+      putStrLn $
+        "Usage: tenhou-dl <Tenhou ID> <Log path>\n"
+          ++ "Example: tenhou-dl ID12345678-6fnB8AoP \"C:\\tenhou\\logs\\\""
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,103 +4,89 @@ module Lib where
 
 import Control.Concurrent.Lock (Lock)
 import qualified Control.Concurrent.Lock as Lock
+import Control.Lens (view, (.~), (^.))
 import Control.Monad.Catch (catchAll)
-import Control.Monad.Except (MonadIO (liftIO), liftEither, runExceptT)
 import qualified Control.Monad.Parallel as P
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import Data.Default (Default (def))
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
+import Data.Function ((&))
 import Data.Maybe (catMaybes)
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
-import GHC.IO.Exception (IOException (ioe_description))
-import Network.HTTP.Req (
-    BsResponse,
-    GET (GET),
-    NoReqBody (NoReqBody),
-    bsResponse,
-    https,
-    parseUrlHttps,
-    req,
-    responseBody,
-    runReq,
-    (/:),
-    (=:),
- )
+import Data.Text.Encoding (decodeUtf8)
+import Network.Wreq (Response, get, responseBody)
+import qualified Network.Wreq as W
 import System.Directory (createDirectoryIfMissing, doesFileExist)
 import System.FilePath ((</>))
-import System.IO.Error (catchIOError)
 import Text.HTML.TagSoup (Tag, fromAttrib, fromTagText, parseTags, sections, (~==))
 
 newtype TenhouID = TenhouID {getTenhouID :: Text}
-    deriving (Show)
+  deriving (Show)
 
 newtype Url = Url {getUrl :: Text}
-    deriving (Show)
+  deriving (Show)
 
-getResponse :: TenhouID -> IO BsResponse
+getResponse :: TenhouID -> IO (Response ByteString)
 getResponse (TenhouID tenhouId) =
-    runReq def $
-        req
-            GET
-            (https "tenhou.net" /: "0" /: "log" /: "find.cgi")
-            NoReqBody
-            bsResponse
-            ("un" =: tenhouId)
+  let options = W.defaults & W.param "un" .~ [tenhouId]
+   in W.getWith options "https://tenhou.net/0/log/find.cgi"
 
-parseResponseTags :: BsResponse -> [Tag ByteString]
-parseResponseTags = parseTags . responseBody
+parseResponseTags :: Response ByteString -> [Tag ByteString]
+parseResponseTags = parseTags . view responseBody
 
 getTags :: TenhouID -> IO [Tag ByteString]
 getTags = fmap parseResponseTags . getResponse
 
 parseDownloadUrls :: [Tag ByteString] -> [Url]
-parseDownloadUrls tags = map (Url . decodeUtf8 . aHref) downloadLinks
-  where
-    aSections = sections (~== ("<a>" :: String)) tags
-    downloadLinks = filter ((== "DOWNLOAD") . fromTagText . (!! 1)) aSections
-    aHref = BS.append "https://tenhou.net" . fromAttrib "href" . head
+parseDownloadUrls tags =
+  map
+    ( Url
+        . decodeUtf8
+        . BS.toStrict
+        . aHref
+    )
+    downloadLinks
+ where
+  aSections = sections (~== ("<a>" :: String)) tags
+  downloadLinks = filter ((== "DOWNLOAD") . fromTagText . (!! 1)) aSections
+  aHref = BS.append "https://tenhou.net" . fromAttrib "href" . head
 
 downloadReplay :: Lock -> Url -> FilePath -> IO (Either String (Maybe FilePath))
 downloadReplay lock url path =
-    runExceptT
-        ( do
-            fileName <- liftEither $ fileNameFromUrl url
-            let subdir = T.take 6 fileName
-            let downloadPath = path </> T.unpack subdir
-            let fullPath = downloadPath </> T.unpack fileName
-            needsDownload <- liftIO $ shouldDownload fullPath
-            if needsDownload
-                then do
-                    liftIO $ createDirectoryIfMissing True downloadPath
-                    replay <- liftIO (getResponseFromUrl url) >>= liftEither
-                    liftIO $
-                        Lock.with lock $
-                            putStrLn $
-                                T.unpack (getUrl url)
-                                    ++ " ==>\n  "
-                                    ++ fullPath
-                    liftIO $ BS.writeFile fullPath $ responseBody replay
-                    return $ Just fullPath
-                else return Nothing
-        )
+  let efileName = fileNameFromUrl url
+   in case efileName of
+        Left e -> return $ Left e
+        Right fileName -> do
+          let subdir = T.take 6 fileName
+          let downloadPath = path </> T.unpack subdir
+          let fullPath = downloadPath </> T.unpack fileName
+          needsDownload <- shouldDownload fullPath
+          if needsDownload
+            then do
+              createDirectoryIfMissing True downloadPath
+              replay <- getResponseFromUrl url
+              Lock.with lock $
+                putStrLn $
+                  T.unpack (getUrl url)
+                    ++ " ==>\n  "
+                    ++ fullPath
+              BS.writeFile fullPath $ replay ^. responseBody
+              return $ Right $ Just fullPath
+            else return $ Right Nothing
         `catchAll` \e -> return $ Left $ show e
 
-getResponseFromUrl :: Url -> IO (Either String BsResponse)
-getResponseFromUrl (Url url) = case parseUrlHttps (encodeUtf8 url) of
-    Just (u, s) ->
-        sequence $ Right $ runReq def $ req GET u NoReqBody bsResponse s
-    Nothing -> return $ Left $ "Error parsing url: " ++ show url
+getResponseFromUrl :: Url -> IO (Response ByteString)
+getResponseFromUrl (Url url) = get $ T.unpack url
 
 downloadReplays :: [Url] -> FilePath -> IO [FilePath]
 downloadReplays urls path = do
-    lock <- Lock.new
-    catMaybes
-        <$> P.mapM (\u -> downloadReplay lock u path >>= unwrapOrPrintError lock) urls
+  lock <- Lock.new
+  catMaybes
+    <$> P.mapM (\u -> downloadReplay lock u path >>= unwrapOrPrintError lock) urls
 
-unwrapOrPrintError :: Monoid a => Lock -> Either String a -> IO a
-unwrapOrPrintError lock (Left e) = Lock.with lock $ putStrLn ("*** " ++ e) >> return mempty
+unwrapOrPrintError :: Lock -> Either String (Maybe FilePath) -> IO (Maybe FilePath)
+unwrapOrPrintError lock (Left e) =
+  Lock.with lock $ putStrLn ("*** " ++ e) >> return Nothing
 unwrapOrPrintError _ (Right p) = return p
 
 maybeToEither :: b -> Maybe a -> Either b a
@@ -108,17 +94,17 @@ maybeToEither = flip maybe Right . Left
 
 fileNameFromUrl :: Url -> Either String Text
 fileNameFromUrl (Url url) = do
-    let splitUrl = T.splitOn "?" url
-    queryParams <- case splitUrl of
-        (_ : ps : _) -> Right ps
-        _ -> Left $ "Error getting query parameters from url: " ++ show url
-    logName <-
-        maybeToEither
-            ( "Error getting log name from query parameters: "
-                ++ T.unpack queryParams
-            )
-            $ T.stripPrefix "log=" queryParams
-    return $ T.append logName ".mjlog"
+  let splitUrl = T.splitOn "?" url
+  queryParams <- case splitUrl of
+    (_ : ps : _) -> Right ps
+    _ -> Left $ "Error getting query parameters from url: " ++ show url
+  logName <-
+    maybeToEither
+      ( "Error getting log name from query parameters: "
+          ++ T.unpack queryParams
+      )
+      $ T.stripPrefix "log=" queryParams
+  return $ T.append logName ".mjlog"
 
 shouldDownload :: FilePath -> IO Bool
 shouldDownload path = not <$> doesFileExist path
